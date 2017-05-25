d92867d46d68a44968a80413f8407750dc73f013
Abzeede <pvongviseskij@yahoo.com>
2017-05-25 10:29:16 +0700

    Remove unused component

--- components/SmoothScrollContainer.js
+++ components/SmoothScrollContainer.js
@@ -5,7 +5,6 @@ eslint
 */
 
 import { scroller } from 'react-scroll'
-import ScrollToTopButton from './ScrollToTopButton'
 
 const isKeyPressedWithClick = (event) => event.ctrlKey || event.altKey || event.shiftKey || event.metaKey
 const isExternal = (targetUrl) => {
@@ -36,7 +35,6 @@ export default function SmoothScrollContainer ({ children }) {
   return (
     <div onClick={handleLinkClick}>
       {children}
-      <ScrollToTopButton />
     </div>
   )
 }
